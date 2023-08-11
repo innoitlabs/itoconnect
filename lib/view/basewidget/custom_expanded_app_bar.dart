957b6fb2f81cfb0ba11e04b5a7c8573acdd3689a
netfreaker <rahulkavati0406@gmail.com>
2023-08-11 13:51:45 +0530

    bloc setup

--- lib/view/basewidget/custom_expanded_app_bar.dart
+++ lib/view/basewidget/custom_expanded_app_bar.dart
@@ -1,58 +1,58 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:i2connect/provider/auth_provider.dart';
-import 'package:i2connect/provider/splash_provider.dart';
-import 'package:i2connect/provider/theme_provider.dart';
-import 'package:i2connect/util/color_resources.dart';
-import 'package:i2connect/util/custom_themes.dart';
-import 'package:i2connect/util/dimensions.dart';
-import 'package:i2connect/util/images.dart';
-import 'package:i2connect/view/basewidget/not_loggedin_widget.dart';
-import 'package:provider/provider.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:i2connect/provider/auth_provider.dart';
+// import 'package:i2connect/provider/splash_provider.dart';
+// import 'package:i2connect/provider/theme_provider.dart';
+// import 'package:i2connect/util/color_resources.dart';
+// import 'package:i2connect/util/custom_themes.dart';
+// import 'package:i2connect/util/dimensions.dart';
+// import 'package:i2connect/util/images.dart';
+// import 'package:i2connect/view/basewidget/not_loggedin_widget.dart';
+// import 'package:provider/provider.dart';
 
-class CustomExpandedAppBar extends StatelessWidget {
-  final String? title;
-  final Widget child;
-  final Widget? bottomChild;
-  final bool isGuestCheck;
-  const CustomExpandedAppBar({Key? key, required this.title, required this.child, this.bottomChild, this.isGuestCheck = false}) : super(key: key);
+// class CustomExpandedAppBar extends StatelessWidget {
+//   final String? title;
+//   final Widget child;
+//   final Widget? bottomChild;
+//   final bool isGuestCheck;
+//   const CustomExpandedAppBar({Key? key, required this.title, required this.child, this.bottomChild, this.isGuestCheck = false}) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    bool isGuestMode = !Provider.of<AuthProvider>(context, listen: false).isLoggedIn();
+//   @override
+//   Widget build(BuildContext context) {
+//     bool isGuestMode = !Provider.of<AuthProvider>(context, listen: false).isLoggedIn();
 
-    return Scaffold(
-      floatingActionButton: isGuestCheck ? isGuestMode ? null : bottomChild : bottomChild,
-      body: Stack(children: [
+//     return Scaffold(
+//       floatingActionButton: isGuestCheck ? isGuestMode ? null : bottomChild : bottomChild,
+//       body: Stack(children: [
 
-        // Background
-        Image.asset(
-          Images.morePageHeader, height: 150, fit: BoxFit.fill, width: MediaQuery.of(context).size.width,
-          color: Provider.of<ThemeProvider>(context).darkTheme ? Colors.black : Theme.of(context).primaryColor,
-        ),
+//         // Background
+//         Image.asset(
+//           Images.morePageHeader, height: 150, fit: BoxFit.fill, width: MediaQuery.of(context).size.width,
+//           color: Provider.of<ThemeProvider>(context).darkTheme ? Colors.black : Theme.of(context).primaryColor,
+//         ),
 
-        Positioned(
-          top: 40,
-          left: Dimensions.paddingSizeSmall,
-          right: Dimensions.paddingSizeSmall,
-          child: Row(children: [
-            CupertinoNavigationBarBackButton(color: Colors.white, onPressed: () {
-              Provider.of<SplashProvider>(context, listen: false).setFromSetting(false);
-              Navigator.pop(context);
-            } ),
-            Text(title!, style: titilliumRegular.copyWith(fontSize: 20, color: Colors.white), maxLines: 1, overflow: TextOverflow.ellipsis),
-          ]),
-        ),
+//         Positioned(
+//           top: 40,
+//           left: Dimensions.paddingSizeSmall,
+//           right: Dimensions.paddingSizeSmall,
+//           child: Row(children: [
+//             CupertinoNavigationBarBackButton(color: Colors.white, onPressed: () {
+//               Provider.of<SplashProvider>(context, listen: false).setFromSetting(false);
+//               Navigator.pop(context);
+//             } ),
+//             Text(title!, style: titilliumRegular.copyWith(fontSize: 20, color: Colors.white), maxLines: 1, overflow: TextOverflow.ellipsis),
+//           ]),
+//         ),
 
-        Container(
-          margin: const EdgeInsets.only(top: 120),
-          decoration: BoxDecoration(
-            color: ColorResources.getHomeBg(context),
-            borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-          ),
-          child: isGuestCheck ? isGuestMode ? const NotLoggedInWidget() : child : child,
-        ),
-      ]),
-    );
-  }
-}
+//         Container(
+//           margin: const EdgeInsets.only(top: 120),
+//           decoration: BoxDecoration(
+//             color: ColorResources.getHomeBg(context),
+//             borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
+//           ),
+//           child: isGuestCheck ? isGuestMode ? const NotLoggedInWidget() : child : child,
+//         ),
+//       ]),
+//     );
+//   }
+// }
